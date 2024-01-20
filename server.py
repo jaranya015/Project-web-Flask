8ac80c364cfe34594892c1f750206819bec2776f
Jaranya Boonthong <faiijaranya0159@gmail.com>
2024-01-20 20:55:45 +0700

    move file

--- server.py
+++ server.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request
 from weather import get_current_weather
 from waitress import serve
 
-
 app = Flask(__name__)
 
 @app.route('/')
@@ -14,12 +13,15 @@ def index():
 def get_weather():
     city = request.args.get('city')
     weather_data = get_current_weather(city)
+
+    title = weather_data.get("name", "Unknown City")
+
     return render_template(
         "weather.html",
-        title = weather_data["name"], 
-        status = weather_data["weather"][0]["description"].capitalize(),
-        temp = f"{weather_data['main']['temp']:.1f}",
-        feels_like = f"{weather_data['main']['feels_like']:.1f}"
+        title=title,
+        status=weather_data["weather"][0]["description"].capitalize(),
+        temp=f"{weather_data['main']['temp']:.1f}",
+        feels_like=f"{weather_data['main']['feels_like']:.1f}"
     )
 
 if __name__ == "__main__" :
